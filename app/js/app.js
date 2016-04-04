fa440ca635b0f68fc8480ff58923ac0103a85b43
p2p-rigid <xinbo.jiang@gmail.com>
2016-04-04 22:32:58 +1000

    app. layout

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 /* App Module */
+
+var albumApp = angular.module("albumSharingApp", [ "ngRoute" ]);
+		
+albumApp.config(function($routeProvider){
+	$routeProvider.when("/albums",{controller: "AlbumController", templateUrl: "album_list_partial.html"})
+			 	  .when("/", {redirectTo: "/albums"})
+				  .when("/404_page", {controller: "Controller_404", templateUrl: "404_page_partial.html"})
+				  .otherwise({redirectTo: "404_page"});  		
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,22 @@
 'use strict';
 
 /* Controllers */
+(function(){
+	function AlbumController($scope){
+		$scope.new_album = {};
+		$scope.albums = [{name:'Photoes for Melbourn',title:'Melbourn trip',date:'2015-10-12', description:'A wonderful trip'},{name:'Photoes for Brisbane',title:'Brisbane trip',date:'2015-10-12', description:'A wonderful trip'},{name:'Photoes for Perth',title:'Melbourn trip',date:'2015-10-12', description:'A wonderful trip'},{name:'Photoes for Canberra',title:'Melbourn trip',date:'2015-10-12', description:'A wonderful trip'}];
+		$scope.add_album = function(new_album) {
+			console.log(new_album);
+			$scope.albums.push(new_album);
+			$scope.new_album = {};
+		}	
+			
+	}
+
+	function Controller_404($scope){
+		$scope.error_message = "oops, not working...";
+	}
+
+	albumApp.controller("AlbumController", AlbumController);
+	albumApp.controller("Controller_404", Controller_404);
+})();
