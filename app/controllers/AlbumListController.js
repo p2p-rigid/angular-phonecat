723bac43671120515b4153ab08710711c37c015e
p2p-rigid <xinbo.jiang@gmail.com>
2016-06-04 18:59:21 +1000

    added some controller

--- app/controllers/AlbumListController.js
+++ app/controllers/AlbumListController.js
@@ -8,7 +8,7 @@
 		$scope.albums = [{name:'Photoes for Melbourn',title:'Melbourn trip',date:'2015-10-12', description:'A wonderful trip'},{name:'Photoes for Brisbane',title:'Brisbane trip',date:'2015-10-12', description:'A wonderful trip'},{name:'Photoes for Perth',title:'Melbourn trip',date:'2015-10-12', description:'A wonderful trip'},{name:'Photoes for Canberra',title:'Melbourn trip',date:'2015-10-12', description:'A wonderful trip'}];
 		$scope.add_album = function(new_album) {
 			$scope.validate(new_album);
-			if(isEmpty($scope.add_album_error)) {
+			if(album_util.isEmpty($scope.add_album_error)) {
 				$scope.albums.push(new_album);
 				$scope.new_album = {};
 		    }
@@ -16,35 +16,21 @@
 
 		$scope.validate = function(album) {
 			$scope.add_album_error="";
-			if(isEmpty(album.title)) {
+			if(album_util.isEmpty(album.title)) {
 				$scope.add_album_error = "Missing album title";
-			} else if (isEmpty(album.name)) {
+			} else if (album_util.isEmpty(album.name)) {
 				$scope.add_album_error = "Missing album name";
-			} else if (isEmpty(album.description)) {
+			} else if (album_util.isEmpty(album.description)) {
 				$scope.add_album_error = "Missing album description";
-			} else if (isEmpty(album.date)) {
+			} else if (album_util.isEmpty(album.date)) {
 				$scope.add_album_error = "Missing album date";
 			}
 		}
 			
 	}
-	
-	/*
-	function Controller_404($scope){
-		$scope.error_message = "oops, not working...";
-	}
-	*/
 
 	albumApp.controller("AlbumController", AlbumController);
 	albumApp.controller("Controller_404", function($scope, $http){
 		$scope.error_message = "oops, not working...";
 	});
-
-	function isEmpty(str) {
-    	return (!str || 0 === str.length);
-	}
-
-	function albumValidator(album) {
-		return false;
-	}
 })();
